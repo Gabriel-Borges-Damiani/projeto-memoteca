290fd6b2550a0e7b37b700c39376680cccfd06a6
Gabriel Borges Damiani <damiani.gabri70@gmail.com>
2026-03-19 09:01:26 -0300

    Adiciona logica pra chamar POST request ao submeter formulario

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-import ui from "./ui.js"
+import ui from "./ui.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   ui.renderizaDadosBack();
+
+  const formularioPensamento = document.getElementById("pensamento-form");
+
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario);
 });
+
+async function manipularSubmissaoFormulario(event) {
+  event.preventDefault();
+  const id = document.getElementById("pensamento-id").value;
+  const conteudo = document.getElementById("pensamento-conteudo").value;
+  const autoria = document.getElementById("pensamento-autoria").value;
+
+  try {
+    await api.enviaProBack({ conteudo, autoria });
+    ui.renderizaDadosBack();
+  } catch {
+    alert("Erro ao salvar pensamento");
+  }
+}
