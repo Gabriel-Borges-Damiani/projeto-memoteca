e227f49cffe6eb09328597daf7f99aa134a8dd30
Gabriel Borges Damiani <damiani.gabri70@gmail.com>
2026-03-19 08:39:59 -0300

    Adiciona POST request

--- js/api.js
+++ js/api.js
@@ -1,14 +1,29 @@
 const api = {
+  async recebeDoBack() {
+    try {
+      const resposta = await fetch("http://localhost:3000/pensamentos");
+      return await resposta.json();
+    } catch (error) {
+      alert("Falha ao pegar dados do servidor");
+      throw error;
+    }
+  },
 
-    async recebeDoBack() {
-        try {
-          const resposta = await fetch('http://localhost:3000/pensamentos');
-          return await resposta.json();
-        } catch (error) {
-          alert("Falha ao pegar dados do servidor");
-          throw error;
-        }
-   }
+  async enviaProBack(pensamento) {
+    try {
+      const resposta = await fetch("http://localhost:3000/pensamentos", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(pensamento),
+      });
+      return await resposta.json();
+    } catch (error) {
+      alert("Falha ao enviar dados pro servidor");
+      throw error;
+    }
+  },
 };
 
 export default api;
